bfdf1c9f19f4b278945b174d40ba440253ce361b
Rafael Gaitán <rgaitan@topcon.com>
2019-04-10 09:36:54 +0200

    Improved script for building with cuda

--- run-detector.sh
+++ run-detector.sh
@@ -3,32 +3,48 @@
 CURRENT_DIR=$(pwd -P)
 WEIGHTS='yolov3.weights'
 CFG='cfg/yolov3.cfg'
+GPU=$(command -v nvcc >/dev/null && echo 1 || echo 0)
 
-function prepare_yolo {
+function build_yolo {
     if [ -e "darknet/darknet" ]; then
         return
     fi
     git clone https://github.com/pjreddie/darknet
-    cd darknet;
-    make
-    wget https://pjreddie.com/media/files/yolov3.weights
-    cd ..;
+    cd $CURRENT_DIR/darknet;
+    if [ $GPU -eq 1 ]; then
+	echo "Building with GPU"
+        sed -i 's/GPU=0/GPU=1/g' Makefile
+    fi
+    make -j 9
+    cd $CURRENT_DIR
+}
+
+function prepare_yolo {
+    build_yolo
+    cd $CURRENT_DIR/darknet;
+    wget -nc https://pjreddie.com/media/files/yolov3.weights
+    cd $CURRENT_DIR
 }
 
 function run_yolo {
+    if [ ! -e "darknet/darknet" ]; then
+	echo "Yolo executable not found"
+        return
+    fi
     mkdir predictions
     local images=$(ls $1/*)
-    cd darknet;
+    cd $CURRENT_DIR/darknet;
     for image in $images;
     do
         ./darknet detect $CFG $WEIGHTS $image
         name=$(basename $image)
         mv predictions.jpg ../predictions/predictions_$name
     done
-    cd -;
+    cd $CURRENT_DIR
 }
 
 function download_validation_images {
+    cd $CURRENT_DIR
     if [ -d "validation" ]; then
         return
     fi
@@ -42,4 +58,4 @@ prepare_yolo
 download_validation_images
 validation_path="$CURRENT_DIR/validation"
 run_yolo "$validation_path"
-
+cd $CURRENT_DIR
